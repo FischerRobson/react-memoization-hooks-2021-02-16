95266b160ba194cbbdb4df42760f11848673ea0b
Fischer <fischerrobson@gmail.com>
2022-01-24 20:47:04 -0300

    uso dos hooks useMemo e useCallback e memo de componentes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,64 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { HeadCounter } from "./Components/Counter";
 import { FabButton } from "./Components/FabButton";
 import Navbar from "./Components/Navbar";
+import { RepositoryList } from "./Components/RepositortList";
 import { likesCounter } from "./Services/expensiveCalculation";
+
+const SEARCH = "https://api.github.com/search/repositories";
 
 function App() {
   const [totalLikes, setTotalLikes] = useState(0);
   const [dark, setDark] = useState(false);
 
-  const likes = likesCounter(totalLikes);
+  /*
+    Por se tratar de uma função muito pesada, sempre que houver
+    qualquer renderização do componete, a função será recalculada,
+    por exemplo, ao alterar o tema será apresentado delay:
 
-  const theme = {
+    const likes = likesCounter(totalLikes);
+
+    o useMemo (memorize) verifica se o valor na memória foi alterado, somente
+    caso tenha sido ele irá calcular novamente a função.
+  */
+
+  const likes = useMemo(() => likesCounter(totalLikes), [totalLikes]);
+
+  /*
+    A cada render a função é recalculada:
+
+    const getRepositories = () => fetch(`${SEARCH}?q=facebook`);
+
+    ou seja, a cada alteração de estado, ele fará o fetch
+
+  */
+
+  const getRepositories = useCallback((query) => {
+    return fetch(`${SEARCH}?q=${query}`);
+  }, []);
+
+  const theme = useMemo(() => ({
     color: dark ? "#fff" : "#333",
     navbar: dark ? "#1a202c" : "#e5e7eb",
     backgroundColor: dark ? "#333" : "#fff",
-  };
+  }), [dark]);
 
   const toogleDarkmode = () => setDark(!dark);
+
+  /*
+    Sem utilizar o useMemo no theme, o console.log do useEffect é disparado
+    todas as vezes, pois o theme é alterado em todas as renderizações.
+  */
+
+  useEffect(() => {
+    console.log('')
+  }, [theme]);
 
   return (
     <div style={theme} className="App">
       <Navbar theme={theme.navbar} toogleDarkmode={toogleDarkmode} />
       <HeadCounter likes={likes} />
+      <RepositoryList getRepositories={getRepositories} />
       <FabButton totalLikes={totalLikes} setTotalLikes={setTotalLikes} />
     </div>
   );
--- src/Components/RepositortList.js
+++ src/Components/RepositortList.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { memo } from "react";
 import { Repository } from "./Repository";
 
-export const RepositoryList = React.memo(({ getRepositories }) => {
+/*
+  o memo engloba o componente e previne que ele seja alterado apenas
+  pela re-renderização do componente pai.
+*/
+
+export const RepositoryList = memo(({ getRepositories }) => {
   const [items, setItems] = React.useState([]);
   const [query, setquery] = React.useState("facebook");
 
